eab398e8c7cda076d39c20799367c4dd1f30e74a
kalyanChowdary2000 <86348772+kalyanChowdary2000@users.noreply.github.com>
2024-09-27 22:13:01 +0530

    updaing cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # Remove conflicting files and directories before deployment
-pm2 stop all
-pm2 delete backend_server
-rm -rf /root/assignment1/
+# pm2 stop all
+# pm2 delete backend_server
+# rm -rf /root/assignment1/
+#!/bin/bash
+
+# Install Node.js and npm
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Install PM2 globally
+sudo npm install -g pm2
