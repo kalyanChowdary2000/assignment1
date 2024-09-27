f94d512ac80feca78eaf463b7b689286672dc4b7
kalyanChowdary2000 <86348772+kalyanChowdary2000@users.noreply.github.com>
2024-09-27 22:25:02 +0530

    clean fix

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,7 @@
 # Remove conflicting files and directories before deployment
 # pm2 stop all
 # pm2 delete backend_server
-# rm -rf /root/assignment1/
+rm -rf /root/assignment1/
 #!/bin/bash
 
 # Install Node.js and npm
